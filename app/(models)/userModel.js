911f67bc14aa639569009ab38ac3dcb905217946
wungthing <wungthingkashungnao123@gmail.com>
2023-11-29 00:46:45 +0530

    updated usermodel

--- app/(models)/userModel.js
+++ app/(models)/userModel.js
@@ -7,6 +7,11 @@ const UserSchema = new Schema({
     required: true,
     unique: true,
   },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
   photos: {
     type: [{ url: String }],
   },
--- app/api/user/route.js
+++ app/api/user/route.js
@@ -1,17 +1,19 @@
 import UserModel from "@/app/(models)/userModel";
-import { auth, useUser } from "@clerk/nextjs";
+import { auth, currentUser } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
 export const POST = async (req) => {
   try {
+    const userData = await currentUser();
+    const email = userData.emailAddresses[0].emailAddress;
     const { userId } = auth();
     const { imgUrl } = await req.json();
 
     const user = await UserModel.findOne({ clerkId: userId });
     if (!user) {
-      console.log("no user yet");
       let newUser = await UserModel({
         clerkId: userId,
+        email: email,
         photos: { url: imgUrl },
       });
       await newUser.save();
@@ -19,19 +21,17 @@ export const POST = async (req) => {
       return NextResponse.json({
         message: "Successfully added",
         data: newUser,
+        userInfo: userData,
       });
     }
     if (user) {
-      console.log("we have an existing user");
-      // let newPhoto = await UserModel({
-      //   photos: { url: imgUrl },
-      // });
       let newPhoto = await UserModel.findOne({ clerkId: userId });
       newPhoto.photos.push({ url: imgUrl });
       await newPhoto.save();
       return NextResponse.json({
         message: "Successfully added new Photo",
         data: newPhoto,
+        userInfo: userData,
       });
     }
   } catch (error) {
